b0c636f5e36170fcbea1a8880923528e0dc8b065
mvogel78 <mandy.vogel@medizin.uni-leipzig.de>
2012-08-08 21:41:22 +0200

    erstes commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sport::Application.config.secret_token = '8e9430653abde07aca72f8c57e3e128f697a3ddf33260c96e81feb9f8ac50820a3b69c2d87db4191fcd3c73a98585b23da34b04d66cdea789efa13c8d31ff43f'
